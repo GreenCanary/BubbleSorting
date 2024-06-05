c7896b90881866e511679261663a7d357d64f555
GreenCanary <148946127+GreenCanary@users.noreply.github.com>
2024-06-05 09:24:47 +0300

    Add files via upload

---
+++ BubbleSorting/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSorting
+{
+    internal class Program
+    {
+        static void bubbleSort(int[] arr, int n)
+        {
+            int i, j, t;
+            bool swap;
+            for (i = 0; i < n - 1; i++)
+            {
+                swap = false;
+                for (j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        t = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = t;
+                        swap = true;
+                    }
+                }
+                if (swap == false)
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[7];
+            int n = arr.Length;
+
+            Console.WriteLine("Type in your array:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            bubbleSort(arr, n);
+
+            Console.WriteLine("Sorted array:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+// Ivanov M.V.
---
+++ BubbleSorting/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("BubbleSorting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BubbleSorting")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c322e247-9bb0-43e1-9a9e-227dbad4ec9d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
